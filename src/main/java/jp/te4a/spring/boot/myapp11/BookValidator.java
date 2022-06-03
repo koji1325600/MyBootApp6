9a9077b23aac0bd47904572ea5c1f8ceeeaddca7
190088 <190088@e0601-18.JC-21.local>
2022-06-03 12:24:00 +0900

    課題11

---
+++ src/main/java/jp/te4a/spring/boot/myapp11/BookValidator.java
@@ -0,0 +1,36 @@
+package jp.te4a.spring.boot.myapp11;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+public class BookValidator implements ConstraintValidator<BookValid,String>{
+	String param;
+	@Override
+	public void initialize(BookValid nv){ param =  nv.param(); }
+	@Override
+	public boolean isValid(String in,ConstraintValidatorContext cxt){
+		if(in == null){
+			return false;
+		}
+		System.out.println(in.equals(param));
+		return !in.equals(param);
+}}
+
+/*@Data
+@NoArgsConstructor
+public class BookForm {
+private Integer id ;
+@NotNull
+@Size(min = 3)
+@BookValid(param = "abc")
+private String title;
+@Size(min = 3, max = 20)
+private String writter;
+private String publisher;
+@Min(0)
+private Integer price;
+}*/
