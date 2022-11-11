d0526cc387134746d76940a1a9c8256dc692812a
koji1325600 <te190088@gmail.com>
2022-11-11 10:58:58 +0900

    fix: 不要なコードを削除

--- src/main/java/jp/te4a/spring/boot/myapp13/validate/WritterValidator.java
+++ src/main/java/jp/te4a/spring/boot/myapp13/validate/WritterValidator.java
@@ -1,12 +1,7 @@
 package jp.te4a.spring.boot.myapp13.validate;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 public class WritterValidator implements ConstraintValidator<Writter,String>{
 	String param;
 	@Override
